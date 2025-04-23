99d3d36fcf21a385f49aff022564c3283a24bc1a
parzivale <40600864+parzivale@users.noreply.github.com>
2025-04-23 13:10:33 +0200

    fix: moved chat code around so it wont kill itself after it begins to listen

--- examples/chat.rs
+++ examples/chat.rs
@@ -1,10 +1,11 @@
 use base64::{Engine as _, engine::general_purpose};
 use core::panic;
-use futures_util::{SinkExt, StreamExt};
+use futures_util::{SinkExt, StreamExt, sink::Send};
 use harmony_core::BrokerBuilder;
 use iroh::{NodeId, SecretKey};
 use keyring::Entry;
 use protocol::message::v1::Message;
+use rand::random;
 use std::{env::args, io::stdin, str::FromStr};
 
 #[tokio::main]
@@ -25,43 +26,44 @@ async fn main() {
             return;
         }
         Ok(secret) => SecretKey::from_bytes(&secret.try_into().unwrap()),
-        Err(err) => panic!("{:?}", err),
+        Err(err) => {
+            let key = SecretKey::generate(rand::rngs::OsRng);
+            let encoded = key.secret().as_bytes();
+            entry.set_secret(encoded).unwrap();
+            panic!("{:?}", err);
+        }
     };
-    let node_id = if let Some(id) = args.into_iter().nth(1) {
-        id
-    } else {
-        let _ = BrokerBuilder::new(key.clone()).await.unwrap();
-        println!("Your current public key is: {:?}", key.public());
-        return;
-    };
-
     println!("I am listening on key: {}", key.public());
-    println!("Connecting to peer: {:?}", node_id);
-    let builder = BrokerBuilder::new(key).await.unwrap();
+    let builder = BrokerBuilder::new(key.clone()).await.unwrap();
 
     let broker = builder.add_protocol::<Message>().build().await.unwrap();
     let send_broker = broker.clone();
     let recieve_broker = broker.clone();
+    if let Some(node_id) = args.into_iter().nth(1) {
+        tokio::spawn(async move {
+            let stdin = stdin();
+            let mut buffer = String::new();
+            let node_id = NodeId::from_str(&node_id).unwrap();
+            let mut send_stream = send_broker
+                .send_packet_sink::<Message>(node_id)
+                .await
+                .unwrap();
+            loop {
+                stdin.read_line(&mut buffer).unwrap();
+                send_stream.send(Message::new(&buffer)).await.unwrap();
+                buffer.clear();
+            }
+        });
+    } else {
+        let _ = BrokerBuilder::new(key.clone()).await.unwrap();
+        println!("Your current public key is: {:?}", key.public());
+    };
+
     tokio::spawn(async move {
         let mut stream = recieve_broker.recieve_packet_stream::<Message>().unwrap();
         while let Some(message) = stream.next().await {
             let message = message.unwrap();
             println!("{}", message.data());
-        }
-    });
-
-    tokio::spawn(async move {
-        let stdin = stdin();
-        let mut buffer = String::new();
-        let node_id = NodeId::from_str(&node_id).unwrap();
-        let mut send_stream = send_broker
-            .send_packet_sink::<Message>(node_id)
-            .await
-            .unwrap();
-        loop {
-            stdin.read_line(&mut buffer).unwrap();
-            send_stream.send(Message::new(&buffer)).await.unwrap();
-            buffer.clear();
         }
     })
     .await
